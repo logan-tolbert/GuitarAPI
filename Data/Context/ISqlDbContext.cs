6e533d53813509e4c26a5e35952266635800e6cc
Logan <lodev205@gmail.com>
2025-03-07 10:06:57 -0600

    :zap: Add asynchronous methods for data loading and saving in ISqlDbContext

--- Data/Context/ISqlDbContext.cs
+++ Data/Context/ISqlDbContext.cs
@@ -2,6 +2,8 @@ namespace GuitarAPI.Data.Context;
 
 public interface ISqlDbContext
 {
+    Task<IEnumerable<T>> LoadDataAsync<T, U>(string sqlStatement, U parameters, string connectionName = "Default", bool isStoredProcedure = false);
     IEnumerable<T> LoadData<T, U>(string sqlStatement, U parameters, string connectionName = "Default", bool isStoredProcedure = false);
+    Task SaveDataAsync<T, U>(string sqlStatement, U parameters, string connectionName = "Default", bool isStoredProcedure = false);
     void SaveData<T, U>(string sqlStatement, U parameters, string connectionName = "Default", bool isStoredProcedure = false);
 }
